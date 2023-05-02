8b537115045697e8002449c34dae163c22be9782
Kamran <mkamran1967@gmail.com>
2023-05-01 19:56:19 -0700

    seeder

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .routers import handler
 import logging
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Routers
 app.include_router(handler.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4333, log_level="info")
